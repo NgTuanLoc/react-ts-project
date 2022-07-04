c1d7f1988999808013262a5d95b33933e3b77790
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-07-04 15:09:08 +0700

    project 5 completed

--- 05-Menu/src/App.tsx
+++ 05-Menu/src/App.tsx
@@ -1,5 +1,77 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import Food, { FoodType } from './components/Food';
+import Category from './components/Category';
+import { data } from './data/data';
+
+const categories = [
+	'all',
+	...Array.from(new Set(data.map((item) => item.category))),
+];
+
 function App() {
-	return <div className='App'>my app</div>;
+	const [Menu, setMenu] = useState<FoodType[]>(data);
+
+	const filterFood = (category: string) => {
+		if (category === 'all') {
+			setMenu(data);
+		} else {
+			const newFood = data.filter((food) => food.category === category);
+			setMenu(newFood);
+		}
+	};
+
+	return (
+		<Wrapper className='App'>
+			<div className='title'>
+				<h1>Our Menu</h1>
+				<div className='underline'></div>
+			</div>
+			<div className='content'>
+				<Category categories={categories} filterFood={filterFood} />
+				<div className='menu'>
+					{Menu.map((food) => (
+						<Food key={food.id} {...food} />
+					))}
+				</div>
+			</div>
+		</Wrapper>
+	);
 }
+
+const Wrapper = styled.main`
+	width: 80vw;
+	max-width: var(--max-width);
+	margin: 5rem auto;
+	.title {
+		text-align: center;
+		margin-bottom: 2rem;
+		h1 {
+			font-size: 4rem;
+		}
+		.underline {
+			background-color: var(--clr-primary);
+			width: 8rem;
+			height: 4px;
+			margin: 0.5rem auto;
+		}
+	}
+
+	.menu {
+		display: grid;
+		grid-gap: 2rem;
+		grid-template-columns: 1fr 1fr;
+	}
+	@media only screen and (max-width: 1200px) {
+		.menu {
+			grid-template-columns: 1fr;
+		}
+		article {
+			grid-template-columns: 30rem 1fr;
+			padding-inline: 10rem;
+		}
+	}
+`;
 
 export default App;
---
+++ 05-Menu/src/components/Category.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+export interface CategoryType {
+	categories: string[];
+	filterFood: (category: string) => void;
+}
+
+const Category: React.FC<CategoryType> = ({ categories, filterFood }) => {
+	const [pickedCategory, setPickedCategory] = useState('all');
+	return (
+		<Wrapper>
+			{categories.map((category, id) => {
+				return (
+					<button
+						className={`${category === pickedCategory ? 'active' : ''}`}
+						key={id}
+						onClick={() => {
+							setPickedCategory(category);
+							filterFood(category);
+						}}>
+						{category}
+					</button>
+				);
+			})}
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled.div`
+	margin-inline: auto;
+	width: 50rem;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	margin-bottom: 3rem;
+	button {
+		background-color: transparent;
+		border: transparent;
+		font-size: 2rem;
+		text-transform: capitalize;
+		font-weight: bold;
+		color: var(--clr-primary);
+		transition: var(--transition);
+		padding: 0.5rem;
+		border-radius: var(--radius);
+		cursor: pointer;
+	}
+	button:hover {
+		background-color: var(--clr-primary);
+		color: white;
+	}
+	.active {
+		background-color: var(--clr-primary);
+		color: white;
+	}
+`;
+
+export default Category;
