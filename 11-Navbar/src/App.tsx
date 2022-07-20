dcad9a8822a596f97b21ad7f8e65cd92f758d711
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-07-20 14:56:27 +0700

    project 11 completed

--- 11-Navbar/src/App.tsx
+++ 11-Navbar/src/App.tsx
@@ -1,174 +1,136 @@
+import { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
-import { loremIpsum, ILoremIpsumParams } from 'lorem-ipsum';
-import { useState, ChangeEvent, ChangeEventHandler } from 'react';
+import {
+	RiFacebookBoxFill,
+	RiInstagramFill,
+	RiGithubFill,
+	RiLinkedinFill,
+} from 'react-icons/ri';
 
-enum Units {
-	words = 'words',
-	paragraphs = 'paragraphs',
-	sentences = 'sentences',
-}
+import { AiOutlineMenu } from 'react-icons/ai';
 
-function App() {
-	const [loremConfig, setLoremConfig] = useState<ILoremIpsumParams>({
-		count: 2, // Number of "words", "sentences", or "paragraphs"
-		format: 'plain', // "plain" or "html"
-		paragraphLowerBound: 3, // Min. number of sentences per paragraph.
-		paragraphUpperBound: 7, // Max. number of sentences per paragarph.
-		random: Math.random, // A PRNG function
-		sentenceLowerBound: 5, // Min. number of words per sentence.
-		sentenceUpperBound: 15, // Max. number of words per sentence.
-		suffix: '\n', // Line ending, defaults to "\n" or "\r\n" (win32)
-		units: Units.paragraphs, // paragraph(s), "sentence(s)", or "word(s)"
-		// words: ['ad', 'su'], // Array of words to draw from
-	});
+import logo from './logo.svg';
+import Sidebar from './Sidebar';
 
-	const [content, setContent] = useState<string>('');
+const App = () => {
+	const [showSidebar, setShowSidebar] = useState(false);
+	const linksContainerRef = useRef<any>(null);
+	const linksRef = useRef<any>(null);
 
-	const onChangeHandler = (
-		e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
-	): void => {
-		const name = e.target.id;
-		const value = name === 'units' ? e.target.value : Number(e.target.value);
-
-		setLoremConfig({
-			...loremConfig,
-			[name]: value,
-		});
+	const toggleSidebar = () => {
+		setShowSidebar(!showSidebar);
 	};
 
-	const generateHandler = () => {
-		setContent(loremIpsum(loremConfig));
-	};
+	useEffect(() => {
+		const linksHeight = linksRef.current.getBoundingClientRect().height;
+
+		if (showSidebar) {
+			linksContainerRef.current.style.height = `${linksHeight}px`;
+		} else {
+			linksContainerRef.current.style.height = '0px';
+		}
+	}, [showSidebar]);
 
 	return (
-		<Container className='App'>
-			<h1>Lorem Ipsum Generator</h1>
-			<InputContainer>
-				<Label htmlFor='count'>Count: </Label>
-				<Input
-					id='count'
-					type='number'
-					value={loremConfig.count}
-					onChange={onChangeHandler}
-				/>
-				<Label htmlFor='paragraphLowerBound'>Paragraph Lower Bound</Label>
-				<Input
-					id='paragraphLowerBound'
-					type='number'
-					value={loremConfig.paragraphLowerBound}
-					onChange={onChangeHandler}
-				/>
-				<Label htmlFor='paragraphUpperBound'>Paragraph Upper Bound</Label>
-				<Input
-					id='paragraphUpperBound'
-					type='number'
-					value={loremConfig.paragraphUpperBound}
-					onChange={onChangeHandler}
-				/>
-				<Label htmlFor='sentenceLowerBound'>Sentence Lower Bound </Label>
-				<Input
-					id='sentenceLowerBound'
-					type='number'
-					value={loremConfig.sentenceLowerBound}
-					onChange={onChangeHandler}
-				/>
-				<Label htmlFor='sentenceUpperBound'>Sentence Upper Bound </Label>
-				<Input
-					id='sentenceUpperBound'
-					type='number'
-					value={loremConfig.sentenceUpperBound}
-					onChange={onChangeHandler}
-				/>
-				<Label htmlFor='units'>Units</Label>
-				<select
-					value={loremConfig.units}
-					name='units'
-					id='units'
-					onChange={onChangeHandler}>
-					<option value='words'>Words</option>
-					<option value='sentences'>Sentences</option>
-					<option value='paragraphs'>Paragraphs</option>
-				</select>
-				<Button onClick={generateHandler}>Generate</Button>
-			</InputContainer>
-			<Content className='content'>
-				{content.split('\n').map((item, id) => {
-					return <p key={id}>{item}</p>;
-				})}
-			</Content>
+		<Container>
+			<div className='nav-header'>
+				<img src={logo} alt='logo' />
+				<button
+					type='button'
+					className={`nav-toggle ${showSidebar && 'active'}`}
+					onClick={toggleSidebar}>
+					<AiOutlineMenu />
+				</button>
+			</div>
+
+			<Sidebar linksContainerRef={linksContainerRef} linksRef={linksRef} />
+			<SocialContainer>
+				<li>
+					<a href='https://github.com/'>
+						<RiFacebookBoxFill />
+					</a>
+				</li>
+				<li>
+					<a href='https://github.com/'>
+						<RiInstagramFill />
+					</a>
+				</li>
+				<li>
+					<a href='https://github.com/'>
+						<RiGithubFill />
+					</a>
+				</li>
+				<li>
+					<a href='https://github.com/'>
+						<RiLinkedinFill />
+					</a>
+				</li>
+			</SocialContainer>
 		</Container>
 	);
-}
+};
 
-const Container = styled.section`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-
-	padding: 5rem;
-	select {
-		background-color: white;
-		padding: 0.5rem;
-		border: none;
-		font-size: 2rem;
-		margin-inline: 1rem;
-	}
-`;
-
-const Button = styled.button`
-	font-size: 2rem;
-	font-weight: bold;
-	letter-spacing: 3px;
-	text-transform: uppercase;
-	background-color: #49a6e9;
-	padding: 1rem 1.5rem;
-	border: none;
-	border-radius: var(--radius);
-	transition: var(--transition);
-	margin-inline: 5rem;
-	cursor: pointer;
-
-	:hover {
-		background-color: #a5d5f8;
-	}
-`;
-
-const InputContainer = styled.div`
-	max-width: 80vw;
-	display: flex;
-	flex-wrap: wrap;
-	align-items: center;
-	justify-content: left;
-`;
-
-const Input = styled.input`
+const Container = styled.nav`
 	background-color: white;
-	padding: 0.5rem;
-	border: none;
-	font-size: 2rem;
-	margin: 1rem;
-`;
+	padding: 1.5rem 2rem;
+	box-shadow: var(--dark-shadow);
 
-const Label = styled.label`
-	color: #063251;
-	font-style: 2.5rem;
-	font-weight: 500;
-	margin: 1rem;
-`;
-
-const Content = styled.section`
-	text-align: center;
-	max-width: 70vw;
-	margin-top: 10rem;
-
-	p {
-		margin-top: 2rem;
-		font-size: 3rem;
+	.nav-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 	}
 
-	@media only screen and (max-width: 1200px) {
-		max-width: 90vw;
+	.nav-toggle {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 2.5rem;
+		color: #49a6e9;
+		background-color: transparent;
+		border: transparent;
+		transition: var(--transition);
+		cursor: pointer;
+
+		:hover {
+			color: black;
+			transform: rotate(90deg);
+		}
+	}
+
+	.active {
+		color: black;
+		transform: rotate(90deg);
+	}
+
+	@media only screen and (min-width: 800px) {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		.nav-toggle {
+			display: none;
+		}
+	}
+`;
+
+const SocialContainer = styled.ul`
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+	li {
+		margin-inline: 0.5rem;
+		svg {
+			transition: var(--transition);
+			color: var(--clr-primary);
+		}
+
+		:hover svg {
+			color: #8bcbf9;
+		}
+	}
+
+	@media only screen and (max-width: 800px) {
+		display: none;
 	}
 `;
 
---
+++ 11-Navbar/src/Sidebar.tsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+const dummyData = [
+	{
+		url: 'https://www.google.com/',
+		title: 'Home',
+	},
+	{
+		url: 'https://www.google.com/',
+		title: 'About',
+	},
+	{
+		url: 'https://www.google.com/',
+		title: 'Projects',
+	},
+	{
+		url: 'https://www.google.com/',
+		title: 'Contact',
+	},
+	{
+		url: 'https://www.google.com/',
+		title: 'Profile',
+	},
+];
+
+const Sidebar = ({ linksContainerRef, linksRef }: any) => {
+	return (
+		<Container ref={linksContainerRef}>
+			<ul ref={linksRef}>
+				{dummyData.map((item, id) => {
+					return (
+						<li key={id}>
+							<a href={item.url}>{item.title}</a>
+						</li>
+					);
+				})}
+			</ul>
+		</Container>
+	);
+};
+
+const Container = styled.div`
+	height: 0;
+	transition: var(--transition);
+	overflow: hidden;
+
+	li {
+		margin-bottom: 1rem;
+	}
+
+	a {
+		color: black;
+		transition: var(--transition);
+		margin-left: 1rem;
+
+		:hover {
+			color: var(--clr-primary);
+		}
+	}
+
+	@media only screen and (min-width: 800px) {
+		height: auto;
+
+		ul {
+			display: flex;
+			justify-content: space-evenly;
+			align-items: center;
+
+			a {
+				margin-inline: 1rem;
+			}
+		}
+	}
+`;
+
+export default Sidebar;
