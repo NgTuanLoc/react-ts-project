f29282fbb3cbcd801d15c74432978657c658ce20
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-07-23 09:49:52 +0700

    project 15 setup

---
+++ 15-Cocktails/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+import { SearchBar } from '../components';
+
+const Home = () => {
+	return (
+		<Container>
+			<SearchBar />
+		</Container>
+	);
+};
+
+const Container = styled.section``;
+
+export default Home;
