943755f0e405bb3ccc3cf09868fa862d4db8dbf0
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-07-29 09:06:57 +0700

    add margin botton

--- 21-MovieDB/src/components/MovieList.tsx
+++ 21-MovieDB/src/components/MovieList.tsx
@@ -19,6 +19,7 @@ const Container = styled.section`
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
 	grid-gap: 2rem;
+	margin-bottom: 5rem;
 `;
 
 export default MovieList;
